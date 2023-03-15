82ad62a869793d435b5875b15c208a1a2e12a2e4
krasickiPawel <252749@student.pwr.edu.pl>
2023-03-15 22:48:49 +0100

    - api core (POST endpoint for b64gesture and expected gesture) - AIController class to manage AI model (interface that need to be finished) - tested via Postman, React and another python app

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+import settings
+from ai_controller import AIController
+
+
+class Gesture(BaseModel):
+    expected_gesture: str
+    gesture: str
+
 
 app = FastAPI()
+ai = AIController(settings.paths.get("model_path"))
+
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Please use valid POST endpoint ({base_url}/gestures/"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/gestures/")
+async def create_photo(gesture: Gesture):
+    is_gesture_correct = ai.is_gesture_correct(gesture.expected_gesture, gesture.gesture)
+    return {"is_gesture_correct": f"{is_gesture_correct}"}
+    # return{"is_gesture_correct": f"{gesture.expected_gesture == gesture.gesture}"}        # for react learning
